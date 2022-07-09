f955f2bee50af8f6cadcf8472c5ec383c8cdd2bf
faisolrr <faisolrizkiromdon@gmail.com>
2022-07-09 09:35:12 +0700

    Mengumpulkan tugas

--- Part2/problem3/problem3.js
+++ Part2/problem3/problem3.js
@@ -1,5 +1,22 @@
 function frog(jumps) {
   // Your code here
+let arr = new Array(jumps.length);
+arr[0] = 0;
+ 
+for (let i = 1; i < jumps.length; i++) {    
+  if (i === 1) {
+    arr[1] = Math.abs(jumps[0] - jumps[1]);
+  } else {
+    arr[i] = Math.min(
+      arr[i - 1] + Math.abs(jumps[i] - jumps[i - 1]),
+      arr[i - 2] + Math.abs(jumps[i] - jumps[i - 2])
+    );
+  }
 }
+ 
+return(arr[jumps.length - 1]);
+}
+console.log(frog([10, 30, 40, 20])) // 30
+console.log(frog([30, 10, 60, 10, 60, 50])) // 40
 
 module.exports = frog;
--- Part2/problem4/problem4.js
+++ Part2/problem4/problem4.js
@@ -1,5 +1,22 @@
 function romanNumerals(value) {
   // Your code here
+  let decimal=[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let roman=['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  let result="";
+  for(let i=0; i<decimal.length; i++){
+    while(decimal[i]<=value){
+      result+=roman[i]
+      value-=decimal[i]
+    }
+  }
+  return result;
 }
+
+
+console.log(romanNumerals(6))    // VI
+console.log(romanNumerals(9))    // IX
+console.log(romanNumerals(23))   // XXIII
+console.log(romanNumerals(2021)) // MMXXI
+console.log(romanNumerals(1646)) // MDCXLVI
 
 module.exports = romanNumerals;
